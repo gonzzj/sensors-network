bbf4ad6eb1fc9a3b2de3119fcc3bc23dbbe6559e
Gonzalo Jusid <gonzalojusid@gmail.com>
2025-04-10 15:43:40 -0300

    feat: refactor

--- src/components/SensorsGridForm/SensorsGridForm.tsx
+++ src/components/SensorsGridForm/SensorsGridForm.tsx
@@ -5,7 +5,6 @@ import Button from '@mui/material/Button'
 import Alert from '@mui/material/Alert'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import CloudUploadIcon from '@mui/icons-material/CloudUpload'
 import DeleteIcon from '@mui/icons-material/Delete'
 
 interface SensorsGridFormProps {
@@ -15,94 +14,54 @@ interface SensorsGridFormProps {
   setThreshold: (value: number) => void
 }
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
-  height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
-  bottom: 0,
-  left: 0,
-  whiteSpace: 'nowrap',
-  width: 1,
-})
-
 export const SensorsGridForm: FC<SensorsGridFormProps> = ({
   sensorGrid,
   setSensorGrid,
   threshold,
   setThreshold,
-}) => {
-  const handleUploadFile = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const fileReader = new FileReader()
-    const files = e.target.files
-
-    if (files) {
-      fileReader.readAsText(files[0], 'UTF-8')
-      fileReader.onload = (e) => {
-        setSensorGrid((e.target?.result as string) || '')
-      }
-    }
-  }
-
-  return (
-    <>
-      <Typography variant="h5">Sensors data</Typography>
-      <TextField
-        label="Sensors"
-        multiline
-        rows={8}
-        value={sensorGrid}
-        onChange={(e) => setSensorGrid(e.target.value)}
-        helperText="A string representing an NxM grid, where each cell can be an integer representing the temperature of that sensor or an X if the sensor is down"
-      />
-      <Box
-        sx={{
-          mt: -2,
-          mb: 2,
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}
+}) => (
+  <>
+    <Typography variant="h5">Sensors data</Typography>
+    <TextField
+      label="Sensors"
+      multiline
+      rows={8}
+      value={sensorGrid}
+      onChange={(e) => setSensorGrid(e.target.value)}
+      helperText="A string representing an NxM grid, where each cell can be an integer representing the temperature of that sensor or an X if the sensor is down"
+    />
+    <Box
+      sx={{
+        mt: -2,
+        mb: 2,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+      }}
+    >
+      <Button
+        color="error"
+        startIcon={<DeleteIcon />}
+        onClick={() => setSensorGrid('[]')}
       >
-        <Button
-          component="label"
-          role={undefined}
-          variant="contained"
-          tabIndex={-1}
-          startIcon={<CloudUploadIcon />}
-        >
-          Upload file
-          <VisuallyHiddenInput
-            type="file"
-            onChange={handleUploadFile}
-            multiple
-          />
-        </Button>
-        <Button
-          color="error"
-          startIcon={<DeleteIcon />}
-          onClick={() => setSensorGrid('[]')}
-        >
-          Clear grid
-        </Button>
-      </Box>
-      <TextField
-        label="Threshold"
-        type="number"
-        helperText="A number T that represents the stability threshold"
-        value={threshold}
-        onChange={(e) => setThreshold(parseInt(e.target.value))}
-      />
-      <Alert severity="info">
-        <Typography variant="caption">
-          Example sensors grid: <br />
-          <code>
-            [ [23, 22, 22, "X", 30], [24, 23, "X", "X", 29], [22, 24, 25, 30,
-            31], ["X", "X", 25, 31, "X"] ]
-          </code>
-        </Typography>
-      </Alert>
-    </>
-  )
-}
+        Clear grid
+      </Button>
+    </Box>
+    <TextField
+      label="Threshold"
+      type="number"
+      helperText="A number T that represents the stability threshold"
+      value={threshold}
+      onChange={(e) => setThreshold(parseInt(e.target.value))}
+    />
+    <Alert severity="info">
+      <Typography variant="caption">
+        Example sensors grid: <br />
+        <code>
+          [ [23, 22, 22, "X", 30], [24, 23, "X", "X", 29], [22, 24, 25, 30, 31],
+          ["X", "X", 25, 31, "X"] ]
+        </code>
+      </Typography>
+    </Alert>
+  </>
+)
